88c79dfd57e5741edf82ec340f5ded27bfda57d3
dokhare <josephdokhare@gmail.com>
2025-06-25 08:11:51 +0100

    Update app.py

--- app.py
+++ app.py
@@ -52,6 +52,3 @@ def survey():
 def thank_you():
     return "<h2>Thank you for submitting your data!</h2>"
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))  # Render provides PORT environment variable
-    app.run(host='0.0.0.0', port=port)  # Must use 0.0.0.0 for Render
